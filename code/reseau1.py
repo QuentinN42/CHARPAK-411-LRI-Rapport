3df7f9bfea313956681167e4046791b6a79aff0f
N42 <quentin.lieumont@gmail.com>
2019-06-06 16:44:31 +0200

    Result

--- code/reseau1.py
+++ code/reseau1.py
@@ -1,22 +1,21 @@
+# imports
 from keras import Sequential, optimizers, layers
 import numpy as np
 from random import random
 
-
-def f(X):                   # la fonction que l'on cherche
-    W = np.array([0.2, 0.8])
+def f(X):                   # La fonction que l'on cherche
+    W = np.array([0.2, 0.8])# Le vecteur poids
     return W @ X            # Produit scalaire
 
-act = "linear"   # fonction d'activation
-n_input = 2      # nombre de neurones
+act = "linear"   # Fonction d'activation
+n_input = 2      # Nombre de neurones
+# generation des questions/reponses attendues
 questions = np.array([np.array([random(), random()])
                     for i in range(10000)])
 reponses  = np.array([f(q) for q in questions])
 
-sgd = optimizers.SGD(lr=0.01, decay=1e-6,
-        momentum=0.9, nesterov=True)
-neurones = layers.Dense(1, activation=act,
-        input_dim=n_input, use_bias=False)
+sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+neurones = layers.Dense(1, activation=act, input_dim=n_input, use_bias=False)
 model = Sequential()            # On cree un reseau
 model.add(neurones)             # On lui ajoute des neurones
 model.compile(optimizer=sgd,    # On compile le tout
